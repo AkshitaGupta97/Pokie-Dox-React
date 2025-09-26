74610bce6781b89b047bceb5fd48dc53573323ea
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-09-26 06:17:17 +0530

    first commit

---
+++ src/Component/Pokedex/Pokedex.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from '../Search/Search'
+
+function Pokedex() {
+  return (
+    <div className='pokedex-wrapper'>
+        <h1>Pokedex</h1>
+        <Search />
+    </div>
+  )
+}
+
+export default Pokedex
---
+++ src/Component/Search/Search.jsx
@@ -0,0 +1,15 @@
+
+
+
+function Search() {
+  return (
+    <div className="search-wrapper">
+        <input id="pekemon-name-search" 
+        type="text" placeholder="Enter Search.." 
+        />
+
+    </div>
+  )
+}
+
+export default Search
